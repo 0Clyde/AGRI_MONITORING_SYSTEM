6397cae2ccd19a7e8b737d2d90c9ac0359440366
0Clyde <clyde.b.verano@gmail.com>
2025-12-11 15:30:38 +0800

    Add files via upload

---
+++ AgriSystemApplication.java
@@ -0,0 +1,11 @@
+package com.agri.agri_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = "com.agri.agri_system")  
+public class AgriSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AgriSystemApplication.class, args);
+    }
+}
